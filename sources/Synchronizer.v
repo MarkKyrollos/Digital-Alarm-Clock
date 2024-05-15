3d3a3e0ba160454c7967dcfe0c2c617d63cca28b
Mark George Kyrollos <98829362+MarkKyrollos@users.noreply.github.com>
2024-05-15 03:18:28 +0300

    updated the synchronizer, added 'rst' and changed the names of variables

--- sources/Synchronizer.v
+++ sources/Synchronizer.v
@@ -1,10 +1,14 @@
 `timescale 1ns / 1ps
 
-module Synchronizer(input SIG, clk, output reg SIG1);
-reg META;
-
-always @ (posedge clk) begin
-META <= SIG;
-SIG1 <= META;
-end
+module Synchronizer(input clk, reset, in, output reg out);
+    reg q1;
+    always@(posedge clk, posedge reset) begin
+         if(reset == 1'b1) begin
+            q1 <= 0;
+         end
+         else begin
+             q1 <= in; // set q1 to input value
+             out <= q1;  // set q1 to be the value for output
+         end
+    end
 endmodule
