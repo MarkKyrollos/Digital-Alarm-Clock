130d63895e5deb3bc792eb234dc6d2c573929b93
Mark George Kyrollos <98829362+MarkKyrollos@users.noreply.github.com>
2024-05-15 03:07:56 +0300

    more updates to debouncer

--- sources/Debouncer.v
+++ sources/Debouncer.v
@@ -4,8 +4,10 @@ module Debouncer(input clk, rst, in, output out);
 
 reg q1,q2,q3;
 
+
+// this block is triggered on the rising edge of clk or rst
 always@(posedge clk, posedge rst) begin
- if(rst == 1'b1) begin
+ if(rst == 1'b1) begin // If reset is high
  q1 <= 0;
  q2 <= 0;
  q3 <= 0;
@@ -16,5 +18,5 @@ else begin
  q3 <= q2;
  end
 end
-assign out = (rst) ? 0 : q1&q2&q3;
+assign out = (rst) ? 0 : q1&q2&q3; // If reset is high, output is 0, else output is the AND of q1, q2, q3 together
 endmodule
