41143eba9551876e0edee85eee9db0a592363825
Mark George Kyrollos <98829362+MarkKyrollos@users.noreply.github.com>
2024-05-18 21:41:40 +0300

    updated the final clock divider

--- sources/ClockDivider.v
+++ sources/ClockDivider.v
@@ -1,25 +1,40 @@
 `timescale 1ns / 1ps
 
-module ClockDivider #(parameter n = 50000000) (input clk,
-                                               input rst,
-                                               output reg clk_out);
+/*******************************************************************
+*
+* Module: ClockDivider.v
+* Project: Digital Alarm Clock
+* Author: Mark Kyrollos - markgeorge99@aucegypt.edu
+* Description: this is the clock divider
+*
+* Change history: 1/5/2024 – 18/5/2024
+*
+*
+**********************************************************************/
 
+
+
+module ClockDivider #(parameter n = 50000000) (input clk,
+input rst,
+output reg clk_out
+); // 1Hz is n = 50000000
     
-reg [31:0] count;
+    reg [31:0] counter;
     
     always @ (posedge clk, posedge rst) begin
-        if (rst == 1'b1) // this is an asynchronous reset
-            count <= 32'b0;
-        else if (count == n-1)
-            count <= 32'b0;
+        if (rst == 1'b1)
+            counter <= 32'b0;
+        else if (counter == n-1)
+            counter <= 32'b0;
         else
-            count <= count + 1; // increment the clock counter
+            counter <= counter + 1; // increment the clock counter
     end
+
 
     always @ (posedge clk, posedge rst) begin
         if (rst)
             clk_out <= 0;
-        else if (count == n-1)
+        else if (counter == n-1)
             clk_out <= ~clk_out;
     end
 endmodule
