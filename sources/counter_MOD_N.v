df7f3f77545d924af1313a3d4c85d19f91213739
Mark George Kyrollos <98829362+MarkKyrollos@users.noreply.github.com>
2024-05-18 21:52:27 +0300

    added the counterMODN

--- sources/counter_MOD_N.v
+++ sources/counter_MOD_N.v
@@ -1,18 +1,34 @@
 `timescale 1ns / 1ps
 
-module counter_MOD_N #(parameter x = 3,parameter  n = 5) (input clk, rst, enable,
-input load,
-input[x-1:0] data,
-output reg [x-1:0] count);
+/*******************************************************************
+*
+* Module: counter_MOD_N.v
+* Project: Digital Alarm Clock
+* Author: Mark Kyrollos - markgeorge99@aucegypt.edu
+* Description: this is a counter that counts up till the value N
+*
+* Change history: 1/5/2024 – 18/5/2024
+*
+*
+**********************************************************************/
+
+
+
+
+module counter_MOD_N #(parameter x = 3,parameter  n = 5) (input clk, rst, enable, load,
+ input[x-1:0] data,
+  output reg [x-1:0] count
+  );
   
-always @(posedge clk, posedge rst) begin
+  
+  always @(posedge clk, posedge rst) begin
     if (rst) // if reset button is enabled, reset the counter
         count <= 0;
     else if (load) 
         count <= data;
-    else if (enable & count == n-1)   //counter set to zero again when reaching (n-1)
+    else if (enable & count == n-1) //counter is reseted when count== n-1
         count <= 0;
     else if (enable) 
-        count <= count + 1; // increment the clock counter
+        count <= count + 1;   // increment the clock counter
   end
 endmodule
