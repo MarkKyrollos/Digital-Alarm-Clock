71746b5d35cebc98879651413c76b5a719b26cfc
Mark George Kyrollos <98829362+MarkKyrollos@users.noreply.github.com>
2024-05-18 21:59:10 +0300

    updated our Synchronizer

--- sources/Synchronizer.v
+++ sources/Synchronizer.v
@@ -1,14 +1,29 @@
 `timescale 1ns / 1ps
 
-module Synchronizer(input clk, reset, in, output reg out);
-    reg q1;
+/*******************************************************************
+*
+* Module: Synchronizer.v
+* Project: Digital Alarm Clock
+* Author: Mark Kyrollos - markgeorge99@aucegypt.edu
+* Description: synchronizer for push button
+*
+* Change history: 1/5/2024 – 18/5/2024
+*
+*
+**********************************************************************/
+
+
+
+
+module Synchronizer(input clk, reset, SIG, output reg SIG1);
+    reg META;
     always@(posedge clk, posedge reset) begin
          if(reset == 1'b1) begin
-            q1 <= 0;
+            META <= 0;
          end
          else begin
-             q1 <= in; // set q1 to input value
-             out <= q1;  // set q1 to be the value for output
+             META <= SIG;
+             SIG1 <= META;
          end
     end
 endmodule
